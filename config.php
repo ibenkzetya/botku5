20c9a99db597e2553d739504dd29daff8590ffdf
ibenkzetya <43585572+ibenkzetya@users.noreply.github.com>
2018-12-06 19:54:33 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
+//jangan ada sepasi
+$config = [
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh5mUruCFbLSTipW8inujf4a62Y-2epy_hZychorOzn93nd2umIGys7h7zq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=9424856c27776402",
+  ];
+
+?>
